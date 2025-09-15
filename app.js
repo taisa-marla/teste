dcae1d8084a68be8344f1162174939a9b05c12ee
Taisa Martins <taisa.marla.martins@gmail.com>
2025-09-15 15:24:05 -0300

    Primeio commit

---
+++ app.js
@@ -0,0 +1,66 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+mensagemInicial();
+
+function exibirTextoNaTela(tag,texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function mensagemInicial(){
+exibirTextoNaTela ("h1" , "Jogo do número secreto");
+exibirTextoNaTela("p", `Escolha um número entre 1 e ${numeroLimite}`); 
+}
+
+function gerarNumeroAleatorio(){
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+    if(quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    }else{
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;   
+    }
+}
+
+function verificarChute(){
+    let chute = document.querySelector("input").value;
+    if(chute == numeroSecreto){
+        exibirTextoNaTela("h1", "Acertou!");
+        let palavraTentativa = tentativas > 1? "tentativas": "tentativa";
+        let mensagem = `Parabéns, você acertou o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela("p", mensagem);
+
+        document.getElementById("reiniciar").removeAttribute("disabled");
+    }else {
+        tentativas++;
+        limparCampo();
+        if(chute>numeroSecreto){
+            exibirTextoNaTela("p", "O número secreto é menor!");
+        }else {
+            exibirTextoNaTela("p", "O número secreto é maior");
+        }
+       
+    }
+}
+
+function limparCampo(){
+    let chute = document.querySelector("input");
+    chute.value = "";
+}
+
+function reiniciarJogo(){
+    numeroSecreto =gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    mensagemInicial();
+    document.getElementById("reiniciar").setAttribute("disabled", true);
+}
+
+
+
